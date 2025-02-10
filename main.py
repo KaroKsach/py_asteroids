43cd2033d6bb4925ca24c7c6ce6512ab97155b50
KaroKsach <kksachikyan@gmail.com>
2025-02-09 23:36:00 -0800

    Added player file, added player to main, updated constants

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import pygame
 from pygame import Color
 from constants import *
+from player import Player
 def main():
     pygame.init()
+
+    player_init_x = SCREEN_WIDTH/2
+    player_init_y = SCREEN_HEIGHT/2
+    player = Player(player_init_x, player_init_y)
+
+
+
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -14,6 +22,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill(Color(0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
     
