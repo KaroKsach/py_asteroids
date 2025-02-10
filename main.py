eab55e392ee65cf6814aa196d4bf8cb5ab68023d
KaroKsach <kksachikyan@gmail.com>
2025-02-10 12:39:15 -0800

    Added asteroid and asteroid field classes, added asteroid field to main with group for tracking

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from pygame import Color
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
 from player import Player
 def main():
@@ -7,12 +9,21 @@ def main():
 
     player_init_x = SCREEN_WIDTH/2
     player_init_y = SCREEN_HEIGHT/2
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
     player = Player(player_init_x, player_init_y)
-
-
+    asteroid_field = AsteroidField()
+    
 
     clock = pygame.time.Clock()
-    dt = 0
+    dt = 60
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -21,8 +32,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill(Color(0, 0, 0))
-        player.draw(screen)
+        
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60)/1000
     
